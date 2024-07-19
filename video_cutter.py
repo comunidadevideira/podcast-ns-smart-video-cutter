3bfe8c849fec572ebc7ce761ce238bfb0e756f00
Samuel Bispo <samuel.bezerrab@gmail.com>
2024-07-19 10:53:25 -0300

    feat: read destination extension from file name

--- video_cutter.py
+++ video_cutter.py
@@ -1,14 +1,15 @@
 import csv
-import subprocess
+import os
 from smart_video_cropper import crop_video_detecing_face, cut_video, extract_first_frame_image
 
 # Define the input video file
-INPUT_VIDEO = 'input_video.mp4'
+INPUT_VIDEO = 'input_video.mov'
 DESTINATION_FOLDER='/Users/samuelbezerrab/Developer/scripts/podcast-smart-video-cutter/exported/'
 CSV_FILE = 'video_segments.csv'
 
 # Read the CSV file
 segments = []
+input_video_extension = os.path.splitext(INPUT_VIDEO)[1]
 
 with open(CSV_FILE, mode='r') as file:
     csv_reader = csv.DictReader(file)
@@ -23,7 +24,7 @@ for segment in segments:
     end = segment['end']
 
     extract_first_frame_image(INPUT_VIDEO, begin, f"{DESTINATION_FOLDER}{title}.jpg")
-    cut_video(INPUT_VIDEO, begin, end, f"{DESTINATION_FOLDER}{title}.mp4")
-    crop_video_detecing_face(f"{DESTINATION_FOLDER}{title}.mp4", f"{DESTINATION_FOLDER}{title}.jpg", f"{DESTINATION_FOLDER}{title}_cropped.mp4")
+    cut_video(INPUT_VIDEO, begin, end, f"{DESTINATION_FOLDER}{title}.{input_video_extension}")
+    crop_video_detecing_face(f"{DESTINATION_FOLDER}{title}.{input_video_extension}", f"{DESTINATION_FOLDER}{title}.jpg", f"{DESTINATION_FOLDER}{title}_cropped.{input_video_extension}")
 
 print("Video segments have been created successfully.")
